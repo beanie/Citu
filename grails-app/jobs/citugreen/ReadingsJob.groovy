7f6fb60858b0f92dbeeb43be54a5c3b8de6c4278
unknown <Beanie@Endor.(none)>
2012-01-27 14:48:24 +0000

    Created watch folder and CSV Heat processor

--- grails-app/jobs/citugreen/ReadingsJob.groovy
+++ grails-app/jobs/citugreen/ReadingsJob.groovy
@@ -5,9 +5,9 @@ import citugreen.ReadingService;
 class ReadingsJob {
 
 	ReadingService readingService
-	
+
 	def cronExpression = "0 0 * * * ?" // run on 0 second 0 minute all the time
-    
+
     def execute() {
         readingService.processWater()
 		readingService.processElec()
--- grails-app/services/citugreen/ReadingService.groovy
+++ grails-app/services/citugreen/ReadingService.groovy
@@ -8,9 +8,9 @@ import groovy.util.XmlSlurper;
 import java.text.*;
 
 class ReadingService {
-	
+
 	static transactional = true
-	
+
 	def processElec() {
 		def fileNames = [
 			"http://20.20.3.20/xmlelectricity3.xml",
@@ -23,7 +23,7 @@ class ReadingService {
 		}
 		log.info("Electricity Readings Parsed OK")
 	}
-	
+
 	def processWater() {
 		def fileNames = [
 			"http://20.20.3.20/xmlwater3.xml",
@@ -38,31 +38,42 @@ class ReadingService {
 			"http://20.20.3.29/xmlwater12.xml",
 			"http://20.20.3.30/xmlwater13.xml"
 		]
-		fileNames.each {file -> 
+		fileNames.each {file ->
 			processXML(file, "water")
 		}
 		log.info("Water Readings Parsed OK")
 	}
-	
+
 	def processHeat() {
-		def fileNames = ["c:\\files\\10-2011B.CSV"]
-		fileNames.each {file -> processHeatXML(file) }
+		def f = new File("c:\\files\\")
+		if( f.exists() ){
+			f.eachFile(){ file->
+			if( !file.isDirectory() )
+				if (file.name.endsWith("CSV")) {
+					processHeatXML(file)
+					file.renameTo(new File("c:\\files\\processed", file.name))
+				}
+			}
+		}
+
+		//def fileNames = ["c:\\files\\10-2011B.CSV"]
+		//fileNames.each {file -> processHeatXML(file) }
 		log.info("Heat Readings Parsed OK")
 	}
-	
+
 	def processXML(fileRef, type) {
-		
+
 		def http = new HTTPBuilder(fileRef)
-		
+
 		try {
-			
+
 			http.request( GET, TEXT ) {
-				
+
 				headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
-				
+
 				request.getParams().setParameter("http.connection.timeout", new Integer(5000));
 				request.getParams().setParameter("http.socket.timeout", new Integer(5000));
-				
+
 				// response handler for a success response code:
 				response.success = { resp, reader ->
 					log.info("** Got "+ type +" XML "+ resp.statusLine +" **")
@@ -72,7 +83,7 @@ class ReadingService {
 						processWaterXML(reader)
 					}
 				}
-				
+
 				// handler for any failure status code:
 				response.failure = { resp -> log.error("Unexpected error: ${resp.status} : ${resp.statusLine.reasonPhrase}")}
 			}
@@ -80,25 +91,25 @@ class ReadingService {
 			log.error(e)
 		}
 	}
-	
+
 	/*
 	 * Process the electricity readings XML
 	 */
 	def processElecXML(fileRef) {
-		
+
 		def readingsXML = new XmlSlurper().parse(fileRef)
 		def readings = readingsXML.group
 		def fileInfo = readingsXML.project
-		
+
 		log.info("*** Parsing Electricity info. for: "+ fileInfo.name +" Created: "+ fileInfo.creationdate +" ***")
-		
+
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd")
 		def tmpFileDate = df.parse(fileInfo.creationdate.toString())
-		
+
 		readings.element.each {reading ->
-			
+
 			def premise = Premise.findByFlatNo(reading.name.toString())
-			
+
 			if (premise) {
 				log.info("** Premise found: "+ premise +" - ELEC: "+ reading.item.valuelong.toString())
 				def tmpReading = new ElecReading(elecReading:reading.item.valuelong.toString(), fileDate:tmpFileDate, premise:premise)
@@ -109,15 +120,15 @@ class ReadingService {
 		}
 		log.info("*** Parsing Electricity info. for: "+ fileInfo.name +" complete ***")
 	}
-	
+
 	/*
 	 * Process the heating readings
 	 */
 	def processHeatXML(fileRef) {
-		
+
 		log.info("*** Parsing Heat info. for: "+ fileRef +" ***")
-		
-		new File(fileRef).splitEachLine(",") {fields ->
+
+		fileRef.splitEachLine(",") {fields ->
 			def premise = Premise.findByFlatNo(fields[0])
 			if (premise){
 				log.info("** Premise found: "+ premise +" - HEAT: "+ fields[1].toString()+" COST: "+ fields[4].toString())
@@ -129,25 +140,25 @@ class ReadingService {
 		}
 		log.info("*** Parsing Heat info. for: "+ fileRef +" complete ***")
 	}
-	
+
 	/*
 	 * Process the water readings XML
 	 */
 	def processWaterXML(fileRef) {
-		
+
 		def readingsXML = new XmlSlurper().parse(fileRef)
 		def readings = readingsXML.group
 		def fileInfo = readingsXML.project
-		
+
 		log.info("*** Parsing Water info. for: "+ fileInfo.name +" Created: "+ fileInfo.creationdate +" ***")
-		
+
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd")
 		def tmpFileDate = df.parse(fileInfo.creationdate.toString())
-		
+
 		readings.element.each {reading ->
-			
+
 			def premise = Premise.findByFlatNo(reading.name.toString())
-			
+
 			if (premise) {
 				log.info("** Premise found: "+ premise +" - COLD WATER: "+ reading.item[0].valuelong.toString()+" HOT WATER: "+ reading.item[1].valuelong.toString()+" GREY WATER: "+ reading.item[2].valuelong.toString())
 				def tmpReading = new WaterReading(fileDate:tmpFileDate, coldWater:reading.item[0].valuelong.toString(), hotWater:reading.item[1].valuelong.toString(), greyWater:reading.item[2].valuelong.toString(), premise:premise)

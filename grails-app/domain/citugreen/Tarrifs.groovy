04cbdcdc72191719748d687f27828c33c2c37db0
Beanie <ben@chilling.co.uk>
2012-01-28 10:46:59 +0000

    Added Tarrifs and tidy up BootStrap

---
+++ grails-app/domain/citugreen/Tarrifs.groovy
@@ -0,0 +1,13 @@
+package citugreen
+
+class Tarrifs {
+	
+	Float coldWaterTarrif
+	Float hotWaterTarrif
+	Float greyWaterTarrif
+	Float elecTarrif
+	Float heatTarrif
+
+    static constraints = {
+    }
+}
---
+++ test/unit/citugreen/TarrifsTests.groovy
@@ -0,0 +1,17 @@
+package citugreen
+
+import grails.test.*
+
+class TarrifsTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}

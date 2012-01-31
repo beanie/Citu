f1cd089b4cb449186c3264b300069fca1b936599
beanie <ben@chilling.co.uk>
2012-01-31 09:14:10 +0000

    added test

---
+++ test/unit/citugreen/MonthViewTests.groovy
@@ -0,0 +1,17 @@
+package citugreen
+
+import grails.test.*
+
+class MonthViewTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/citugreen/WeekHolderTests.groovy
@@ -0,0 +1,17 @@
+package citugreen
+
+import grails.test.*
+
+class WeekHolderTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}

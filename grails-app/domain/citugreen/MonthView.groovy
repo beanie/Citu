bf969577e82c5ad4bdf561caf84bc1ea1aad6155
beanie <ben@chilling.co.uk>
2012-01-31 09:13:46 +0000

    added tests

---
+++ grails-app/domain/citugreen/MonthView.groovy
@@ -0,0 +1,21 @@
+package citugreen
+
+class MonthView {
+	
+	String week1
+	String week2
+	String week3
+	String week4
+	String week5
+	
+	ArrayList elecReadings
+	ArrayList waterReadings
+	ArrayList heatReadings
+	
+	static transients = ['week1','week2','week3','week4','week5', 'elecReadings', 'waterReadings', 'heatReadings']
+
+    static constraints = {
+		week4(nullable:true, blank:true)
+		week5(nullable:true, blank:true)
+    }
+}
---
+++ grails-app/domain/citugreen/WeekHolder.groovy
@@ -0,0 +1,13 @@
+package citugreen
+
+class WeekHolder {
+	
+	String utilityType
+	Float utilityUse
+	Float utilityCost
+	
+	static transients = ['utilityType','utilityUse','utilityCost']
+
+    static constraints = {
+    }
+}

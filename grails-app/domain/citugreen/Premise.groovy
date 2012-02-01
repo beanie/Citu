65fdc43b956967e9e4b0e00155830f4e41ba37f3
beanie <ben@chilling.co.uk>
2012-02-01 15:56:52 +0000

    Merged in Phils changes

--- grails-app/domain/citugreen/Premise.groovy
+++ grails-app/domain/citugreen/Premise.groovy
@@ -1,64 +1,79 @@
 package citugreen
 
 class Premise {
-	
+
 	String flatNo
 	String addressLine1
 	String addressLine2
 	String postCode
 	int rooms
 	int squareArea
-	
+
 	User user
-	
+
 	static hasMany=[elecReadings:ElecReading,waterReadings:WaterReading,heatReadings:HeatReading,setTobBoxes:SetTopBox]
-	
+
 	static mapping = {
 		elecReadings sort:'fileDate'
 		heatReadings sort:'fileDate'
 		waterReadings sort:'dateCreated'
 	}
-	
+
 	Float getTotalElecUsage() {
 		BillUtils.calcTotal(this.elecReadings.elecReading)
 	}
-	
+
 	Float getTotalHeatUsage() {
 		BillUtils.calcTotal(this.heatReadings.heatReading)
 	}
-	
+
+
 	Float getTotalColdWater() {
 		BillUtils.calcTotal(this.waterReadings.coldWater)
 	}
-	
+
 	Float getTotalHotWater() {
 		BillUtils.calcTotal(this.waterReadings.hotWater)
 	}
-	
+
 	Float getTotalGreyWater() {
 		BillUtils.calcTotal(this.waterReadings.greyWater)
 	}
-	
-	
+
+
 	Float getTotalHeatCost() {
-		def tmp = BillUtils.calcTotalHeatCost(this.heatReadings.heatReading)
-		
+		BillUtils.calcTotalHeatCost(this.heatReadings.heatReading)
+
 	}
-	
+
 	Float getTotalElecCost() {
-		BillUtils.calcTotal(this.elecReadings.elecReading)
+		BillUtils.calcTotalElecCost(this.elecReadings.elecReading)
 	}
-	
-	Float getAveColdWater() {
-		BillUtils.aveColdTotalbyRoom(this.waterReadings.coldWater, this.rooms)
+
+	Float getTotalHotWaterCost() {
+		BillUtils.calcTotalHotWaterCost(this.waterReadings.hotWater)
 	}
-	
-	
+
+	Float getTotalColdWaterCost() {
+		BillUtils.calcTotalColdWaterCost(this.waterReadings.coldWater)
+	}
+
+	Float getTotalGreyWaterCost() {
+		BillUtils.calcTotalGreyWaterCost(this.waterReadings.greyWater)
+	}
+
+
+	Float getAveHotWaterByRoom() {
+		BillUtils.aveHotWaterByRoom(this.waterReadings.hotWater, this.rooms)
+	}
+
+
 	String toString() {
 		return flatNo
 	}
-	
-	static transients = ['totalHeatUsage','totalElecUsage','totalElecCost', 'totalHeatCost','totalHotWater','totalColdWater','totalGreyWater', 'aveGreyWater', 'aveColdWater']
+
+	static transients = ['totalHeatUsage','totalElecUsage','totalHotWater','totalColdWater','totalGreyWater', ,'totalHeatCost','totalElecCost','totalHotWaterCost',
+		'totalColdWaterCost','totalGreyWaterCost','aveHotWaterByRoom']
 
     static constraints = {
 		addressLine1(blank:false, nullable: false)
--- src/java/citugreen/BillUtils.groovy
+++ src/java/citugreen/BillUtils.groovy
@@ -2,58 +2,58 @@ package citugreen
 
 
 class BillUtils {
-	
+
 	static Float calcTotal(ArrayList costs) {
 		def tmpFloat = 0
 		for (i in costs) {
 			if (i != null) {
 				tmpFloat += i
 			}
-		}		
+		}
 		return tmpFloat
 	}
-	
+
 	static Float calcTotalHeatCost(ArrayList costs){
 		def tmp = calcTotal(costs)
 		println (tmp)
 		def tmpCost = citugreen.Tarrifs.get(1).heatTarrif
 		return (tmp*tmpCost)
 	}
-	
+
 	static Float calcTotalElecCost(ArrayList costs){
 		def tmp = calcTotal(costs)
 		println (tmp)
 		def tmpCost = citugreen.Tarrifs.get(1).elecTarrif
 		return (tmp*tmpCost)
 	}
-	
+
 	static Float calcTotalGreyWaterCost(ArrayList costs){
 		def tmp = calcTotal(costs)
 		println (tmp)
 		def tmpCost = citugreen.Tarrifs.get(1).greyWaterTarrif
 		return (tmp*tmpCost)
 	}
-	
+
 	static Float calcTotalHotWaterCost(ArrayList costs){
 		def tmp = calcTotal(costs)
 		println (tmp)
 		def tmpCost = citugreen.Tarrifs.get(1).hotWaterTarrif
 		return (tmp*tmpCost)
 	}
-	
-	
+
+
 	static Float calcTotalColdWaterCost(ArrayList costs){
 		def tmp = calcTotal(costs)
 		println (tmp)
 		def tmpCost = citugreen.Tarrifs.get(1).coldWaterTarrif
 		return (tmp*tmpCost)
 	}
-		
-	
-	static Float aveColdTotalbyRoom(ArrayList costs, int rooms) {
+
+
+	static Float aveHotWaterByRoom(ArrayList costs, int rooms) {
 		def tmpFloat = calcTotalColdWaterCost(costs)
-		def tmpAve = tmpFloat/rooms
-		return tmpAve
+		def tmpRoomAve = (tmpFloat/rooms)
+		return tmpRoomAve
 	}
 
 }
